bc819dc53c745abef24af1f5d86ded1aa8513120
Suraj Doss <suraj_sebastien@yahoo.com>
2021-02-26 15:02:38 +0530

    Update Day 9 Recursion 3.cpp

--- Day 9 Recursion 3.cpp
+++ Day 9 Recursion 3.cpp
@@ -1,3 +1,39 @@
+//Recursion method
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int n) {
+    int fact;
+    if(n==1){
+        return 1;
+    }else{
+        fact = n * factorial(n-1);
+        return fact;
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+
+//another method w/o recursion
+
 #include <bits/stdc++.h>
 
 using namespace std;
