43c9defb5dda845a3476f4fa01a73b1c81ef7c26
Suraj Doss <suraj_sebastien@yahoo.com>
2021-03-01 20:45:29 +0530

    Update Day 12 Inheritance.cpp

--- Day 12 Inheritance.cpp
+++ Day 12 Inheritance.cpp
@@ -1,26 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-
-class Person{
-	protected:
-		string firstName;
-		string lastName;
-		int id;
-	public:
-		Person(string firstName, string lastName, int identification){
-			this->firstName = firstName;
-			this->lastName = lastName;
-			this->id = identification;
-		}
-		void printPerson(){
-			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
-		}
-	
-};
-
 class Student :  public Person{
 	private:
 		vector<int> testScores;  
@@ -47,30 +24,12 @@ class Student :  public Person{
         if(avgScores >= 70 && avgScores <80){
             return 'A';
         }
-        if(avgScores >= 60 && avgScores <70){
+        if(avgScores >= 55 && avgScores <70){
             return 'P';
         }
-        if (avgScores >=55 && avgScores <60){
+        if (avgScores >=40 && avgScores <55){
             return 'D';
         }
         return 'T';
-    };
+    }
 };
-
-int main() {
-	string firstName;
-  	string lastName;
-	int id;
-  	int numScores;
-	cin >> firstName >> lastName >> id >> numScores;
-  	vector<int> scores;
-  	for(int i = 0; i < numScores; i++){
-	  	int tmpScore;
-	  	cin >> tmpScore;
-		scores.push_back(tmpScore);
-	}
-	Student* s = new Student(firstName, lastName, id, scores);
-	s->printPerson();
-	cout << "Grade: " << s->calculate() << "\n";
-	return 0;
-}
