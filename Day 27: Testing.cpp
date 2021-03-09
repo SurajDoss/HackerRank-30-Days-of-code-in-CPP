6d705f0825c82df36fb6e3515dbc53afe9dbfab9
Suraj Doss <suraj_sebastien@yahoo.com>
2021-03-09 16:22:09 +0530

    Create Day 27: Testing.cpp

---
+++ Day 27: Testing.cpp
@@ -0,0 +1,31 @@
+class TestDataEmptyArray {
+public:
+    static vector<int> get_array() {
+        return{};
+    }
+
+};
+
+class TestDataUniqueValues {
+public:
+    static vector<int> get_array() {
+        return {1,2};
+    }
+
+    static int get_expected_result() {
+        return 0;
+    }
+
+};
+
+class TestDataExactlyTwoDifferentMinimums {
+public:
+    static vector<int> get_array() {
+        return {1,1};
+    }
+
+    static int get_expected_result() {
+        return 0;
+    }
+
+};
